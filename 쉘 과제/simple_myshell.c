92f630df52ba5c79d6c9dea30519ae4a23244e28
박찬준 <pcj01142@gmail.com>
2019-11-10 22:28:26 +0900

    (1) 엔터만 입력하면 세그멘테이션 오류가 발생하는 버그 수정 (2) myshell이 제어키에 의해서 종료되지 않고, 포 그라운드 프로세스는 제어키에 의해 종료될 수 있도록 시그널 처리

--- 쉘 과제/simple_myshell.c
+++ 쉘 과제/simple_myshell.c
@@ -14,7 +14,12 @@
                    에서 호출하게 했다. foreground 명령의 경우 부모 프로세스에서 자식 프로세스의 
                    종료로 발생하는 SIGCHLD가 적절히 처리될때까지 pause함수를 호출하여 
                    대기시킴으로써 구현하였다. background 명령의 경우 pause 함수를 호출하지
-                   않게하여 myshell의 while 구문이 계속해서 실행되도록 하여 구현하였다.
+                   않게하여 myshell의 while 구문이 계속해서 실행되도록 구현하였다.
+    
+    - 2019-11-10 : (1) 엔터만 입력하면 세그멘테이션 오류가 발생하면서 종료되는 버그 수정.
+
+                   (2) myshell이 제어키에 의해서 종료되지 않고, 포그라운드 프로세스는 제어키에
+                   의해 종료될 수 있도록 시그널 처리.
 */
 
 #include <stdio.h>
@@ -31,6 +36,8 @@
 #define MAX_USR_NAME 100
 #define TRUE 1
 #define FALSE 0
+#define FOREGROUND 0
+#define BACKGROUND 1
 
 // const char *prompt = "myshell> ";
 const char *prompt_front = "myshell:";
@@ -45,6 +52,7 @@ void makeprompt();
 int makelist(char *s, const char *delimiters, char** list, int MAX_LIST);
 void catchsigchld(int signo);
 int findhomepath();
+void run_cmd_grp(char** cmd_args, int type);
 
 int main() {
     int i=0;
@@ -54,13 +62,24 @@ int main() {
     pid_t tmp_pid;
     static struct sigaction act;
 
-
     // myshell의 자식 프로세스가 종료하거나 중단되는 경우(SIGCHLD)
     // 필요시 myshell에서 수행해야하는 일들 처리
     act.sa_handler = catchsigchld;
     sigfillset(&(act.sa_mask));         // 처리중에 들어오는 다른 시그널 blocking
     act.sa_flags = SA_RESTART;          // 시스템호출이 시그널 핸들러에 의해 블럭되는 경우 재시작
     sigaction(SIGCHLD, &act, NULL);
+
+    // myshell이 제어키(^C, ^\, ^Z)에 의해서 종료되지 않도록
+    // 다음 시그널들에 대해서 무시하도록 설정
+    act.sa_handler = SIG_IGN;
+    sigemptyset(&(act.sa_mask));
+    
+    sigaction(SIGQUIT, &act, NULL);
+    sigaction(SIGINT, &act, NULL);
+    sigaction(SIGTSTP, &act, NULL);
+
+    // background 프로세스가 터미널에 write를 시도할때 발생
+    signal(SIGTTOU, SIG_IGN);
 
     while (1) {
 
@@ -77,6 +96,9 @@ int main() {
             fprintf(stderr, "명령어가 너무 깁니다.\n");
             continue;
         }
+        else if (numtokens == 0) { // 엔터키만 입력하는 경우 처리
+            continue;
+        }
 
         // shell built-in 명령어들 처리
         if (!strcmp(cmdvector[0], "cd")) {
@@ -88,6 +110,7 @@ int main() {
                 fprintf(stderr, "cd : 인수가 너무 많음\n");
                 continue;
             }
+            // 현재 디렉토리 변경
             if (chdir(cmdvector[1]) == -1) {
                 fprintf(stderr, " %s ", cmdvector[1]);
                 perror(" cd ");                
@@ -98,23 +121,13 @@ int main() {
             exit(0);
         }
 
-	    switch(pid=fork()){
-	    case 0:     // 자식 프로세스(shell 명령어)
-            if (!strcmp(cmdvector[numtokens - 1], "&")) {   // 백그라운드 실행이면
-                cmdvector[numtokens - 1] = NULL;            // 백그라운드 옵션은 myshell에서 처리
-            }
-            execvp(cmdvector[0], cmdvector);      
-		    fatal("main()");
-	    case -1:
-  		    fatal("main()");
-	    default:    // 부모 프로세스(myshell)
-            if (strcmp(cmdvector[numtokens - 1], "&")) {    // 백그라운드 실행이 아닐때 pause를 통해서 동기화시킨다.
-                fgnd_process = pid;
-                while(fgnd_process != 0) {  // foreground 프로세스가 종료되지 않은 경우에 background 프로세스의 
-                    pause();                // 종료에 의해서 pause가 풀리지 않도록 계속해서 pause한다.
-                }
-            }
-	    }
+        if (!strcmp(cmdvector[numtokens - 1], "&")) {   // 백그라운드 실행이면
+            cmdvector[numtokens - 1] = NULL;            // 백그라운드 옵션은 myshell에서 처리
+            run_cmd_grp(cmdvector, BACKGROUND);
+        }
+        else { // 포그라운드 실행
+            run_cmd_grp(cmdvector, FOREGROUND);
+        }
     }
     return 0;
 }
@@ -186,7 +199,7 @@ int findhomepath() {
     int len;
 
     if ( (username = getlogin()) == NULL) {
-        return FALSE;
+        return -1;
     }
     
     strcpy(homedirpath, "/home/");
@@ -194,5 +207,40 @@ int findhomepath() {
     len = strlen(homedirpath);
     strcpy(&homedirpath[len], username);
 
-    return TRUE;
+    return 0;
+}
+
+// myshell에 입력한 명령어를 처리하는 함수
+void run_cmd_grp(char** cmd_args, int type) {
+    pid_t pid = 0;
+    static struct sigaction nact;
+
+    switch(pid=fork()){
+    case 0:     // 자식 프로세스
+        // 새로운 프로세스 그룹 생성
+        setpgid(0, 0);
+        
+        // 자식프로세스는 제어키(^C, ^\, ^Z)에 대해서 디폴트 액션을 취하도록 설정
+        nact.sa_handler = SIG_DFL;
+        sigaction(SIGQUIT, &nact, NULL);
+        sigaction(SIGINT, &nact, NULL);
+        sigaction(SIGTSTP, &nact, NULL);
+        
+        if (type == FOREGROUND) {   // 자식 프로세스가 터미널에 대한 제어권 획득
+            tcsetpgrp(STDIN_FILENO, getpgid(0));
+        }
+
+        execvp(cmdvector[0], cmdvector);      
+        fatal("main()");
+    case -1:
+        fatal("main()");
+    default:    // 부모 프로세스(myshell)
+        if (type == FOREGROUND) {    // 백그라운드 실행이 아닐때 pause를 통해서 동기화시킨다.
+            fgnd_process = pid;
+            while(fgnd_process != 0) {  // foreground 프로세스가 종료되지 않은 경우에 background 프로세스의 
+                pause();                // 종료에 의해서 pause가 풀리지 않도록 계속해서 pause한다.
+            }
+            tcsetpgrp(STDIN_FILENO, getpgid(0)); // 쉘은 기다렸다 제어권 회수
+        }
+    }
 }
